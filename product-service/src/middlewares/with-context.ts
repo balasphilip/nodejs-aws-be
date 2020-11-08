ee5e2eb440a1808163f0ef26f691fa189d763e0e
Pylyp Balasanov <pylyp_balasanov@epam.com>
2020-11-09 00:39:50 +0200

    feat: integrate with RDS to make simple CRUD operations

--- product-service/src/middlewares/with-context.ts
+++ product-service/src/middlewares/with-context.ts
@@ -10,6 +10,9 @@ import { ProductsService } from "@src/services/products";
 
 // dal
 import { Product } from "@src/dal/product/product.entity";
+import { Stock } from "@src/dal/product/stock.entity";
+import { SeedDataIntoProducts1604837745886 } from "@src/migrations/1604837745886-seed-data-into-products";
+import { CreateProducts1604837536330 } from "@src/migrations/1604837536330-create-products-and-seed-data";
 
 import "source-map-support/register";
 
@@ -27,13 +30,22 @@ const handler: WithContextHandler = () => ({
       } catch (error) {
         conn = await createConnection({
           ...connection,
+          migrations: [
+            CreateProducts1604837536330,
+            SeedDataIntoProducts1604837745886,
+          ],
+          entities: [Product, Stock],
           host: process.env.PG_HOST,
           database: process.env.PG_DATABASE,
           port: Number.parseInt(process.env.PG_PORT, 10),
           username: process.env.PG_USERNAME,
           password: process.env.PG_PASSWORD,
         } as ConnectionOptions);
+
+        console.log("Create Connection", { isConnected: conn.isConnected });
       }
+
+      console.log("Create Context");
 
       context = {
         productsService: new ProductsService(conn, conn.getRepository(Product)),
